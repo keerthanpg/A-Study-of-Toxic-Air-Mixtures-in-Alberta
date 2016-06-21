da55cefa3a738ea6d79346105f3c3dddb8c3c8ca
David <David>
2016-06-21 16:46:45 -0600

    Add Dimensions

--- calcnormal.py
+++ calcnormal.py
@@ -1,6 +1,7 @@
 import numpy as np 
 from scipy.stats import multivariate_normal
 import os
+import json
 
 c=np.load('Points.npy')
 for i in xrange(1223):
@@ -11,10 +12,26 @@ for i in xrange(1223):
 		print len(c[i][j])
 		break
 	break
+f=open('EmissionByChemical.txt', 'rb')
+Emissions=json.load(f)
 
-
-var = multivariate_normal(mean=[0,0], cov=[[1,0],[0,1]])
 ChemicalDimensions={}
+i=2
+for j in xrange(2006, 2012):
+	for chemical in Emissions[str(j)]:
+		if chemical in ChemicalDimensions:
+			continue
+		else:
+			ChemicalDimensions[chemical]=i
+			i=i+1
+print i
+print ChemicalDimensions
+
+g=open('ChemicalDimensions', 'wb')
+json.dump(ChemicalDimensions, g)
+'''
+var = multivariate_normal(mean=[0,0], cov=[[1,0],[0,1]])
+
 for i in xrange(2006, 2013):
 	path = '%s/'%str(i)
 	for filename in os.listdir(path):
@@ -23,7 +40,7 @@ for i in xrange(2006, 2013):
 print var.pdf([0,0])
 
 print len(c[0])
-print len(c)
+print len(c)'''
 
 
 '''
