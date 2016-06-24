231c6e7af0a4478a6de4cb49a8e3193db66ef16b
David <David>
2016-06-23 23:08:46 -0600

    Clusters updted

--- cluster.py
+++ cluster.py
@@ -7,9 +7,12 @@ X = np.load("Sets.npy")
 
 print type(X)
 print np.shape(X)
+no_clusters=50
+estimators = {'k_means_8': KMeans(n_clusters=no_clusters)}
 
-estimators = {'k_means_8': KMeans(n_clusters=5)}
-countmembership=[np.zeros(shape=(5))]
+countmembership=[]
+for i in xrange(no_clusters):
+  countmembership.append(0)
 ''','k_means_bad_init': KMeans(n_clusters=3, n_init=1, init='random')'''
 
 
@@ -17,12 +20,13 @@ countmembership=[np.zeros(shape=(5))]
 for name, est in estimators.items():
     est.fit(X[7])
     centers = est.cluster_centers_
-    print np.shape(centers)
-    print est.labels_
     for item in est.labels_:
       countmembership[item]+=1
 
 print countmembership
+for i in xrange(len(countmembership)):
+  if countmembership[i]>50:
+    print i+1
 
     
 f=open('Centers.txt', 'wb')
@@ -36,7 +40,7 @@ ChemicalNames=json.load(h)
 
 for i in xrange(np.shape(centers)[0]):
   for j in xrange(np.shape(centers)[1]):
-    if centers[i][j]>1000:
+    if centers[i][j]>100:
       f.write('(%s:%f) '%(ChemicalNames[ChemicalDimensionInverse[j]],centers[i][j]))
   f.write('\n')
 
