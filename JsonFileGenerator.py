d82919ab5b6da7853a182a9e17a3263187aaa415
keerthanpg <keerthanpg@gmail.com>
2016-06-28 18:17:39 +0530

    Change Mapinit

--- JsonFileGenerator.py
+++ JsonFileGenerator.py
@@ -1,7 +1,11 @@
 import json
 
-name='ChemicalNames'
+name='EmissionByChemical.txt'
 f=open(name, 'rb')
 FacilityCoordinates=json.load(f)
-with open('ChemicalNames.json', 'wb') as fp:
-    json.dump(FacilityCoordinates, fp)
+Reduced={}
+for i in range(2006,2013):
+	Reduced[str(i)]=FacilityCoordinates[str(i)]
+
+with open('EmissionByChemical.json', 'wb') as fp:
+    json.dump(Reduced, fp)
---
+++ jsmain.js
@@ -0,0 +1,44 @@
+function initMap() {
+      	var Emission;
+     	$.getJSON("EmissionByChemical.json", function(json) {
+    	  Emission= json;
+        $.getJSON("FacilityCoordinates.json", function(json) {
+          FacilityCoordinates=json;
+          content=FacilityCoordinates["0000004385"];
+          console.log(content);
+           var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 200,
+          center: {lat: 54.5, lng: -115.0},
+          mapTypeId: google.maps.MapTypeId.TERRAIN
+        });
+
+        // Construct the circle for each value in citymap.
+        // Note: We scale the area of the circle based on the population.
+        for (var emission in Emission['2006']['206-44-00']) {
+          emissioncentre=FacilityCoordinates[emission['NPRI_ID']]
+          // Add the circle for this city to the map.
+          var emissionCircle = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: emissioncentre,
+            radius: emission['Tonnes_Air'] * 1000000
+          });
+
+          
+        }
+
+        });
+    	//document.write(Emission['2006']['7647-01-0'][0]['CHEM_E']);
+    	//content=Emission['2006']['7647-01-0'][0]['CHEM_E'];
+        //window.alert(content);
+		});
+        // Create the map.
+
+       
+        
+        
+      }
