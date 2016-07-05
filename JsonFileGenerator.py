367fdde85f4c81a569e358bbd379f5a56687d75f
keerthanpg <keerthanpg@gmail.com>
2016-07-05 08:46:41 +0530

    Trends in emission

--- JsonFileGenerator.py
+++ JsonFileGenerator.py
@@ -1,14 +1,22 @@
 import json
 
 
+f=open('EmissionByChemical.json', 'rb')
+Emission=json.load(f)
 
-f=open('ChemicalNames.json', 'rb')
-ChemicalNames=json.load(f)
+g=open('Graph.txt', 'ab')
 
-ChemicalNamesInverse={}
+h=open("ChemicalNames", 'rb')
+ChemicalNames=json.load(h)
 
-for k,v in ChemicalNames.iteritems():
-	ChemicalNamesInverse[v]=k
-g=open('ChemicalNumbers.json', 'wb')
-json.dump(ChemicalNamesInverse,g)
+for chemical,name in ChemicalNames.iteritems():	
+	g.write("\n%s; " %name)
+	for i in range(2006,2013):		
+		tonnes=0	
+		if chemical in Emission[str(i)]:
+			for emissions in Emission[str(i)][chemical]:
+				tonnes+=emissions['Tonnes_Air']				
+		g.write("%f; " %tonnes)
+
+g.close()
 
