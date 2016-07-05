48628d83712915db6f2d3c0efa837f291fa1f280
David <David>
2016-07-04 22:01:15 -0600

    Calculate tep

---
+++ CancerEquivalent.py
@@ -0,0 +1,32 @@
+from xlrd import open_workbook
+import json
+
+book_C = open_workbook('teps.xlsx')
+sheet_C = book_C.sheet_by_index(0)
+
+
+# read header values into the list    
+keys = [sheet_C.cell(0, col_index).value for col_index in xrange(sheet_C.ncols)]
+
+cancer_teps = {}
+noncancer_teps = {}
+
+for row_index in xrange(1, sheet_C.nrows):
+    d = {keys[col_index]: sheet_C.cell(row_index, col_index).value 
+         for col_index in xrange(sheet_C.ncols)}
+    e= {keys[col_index]: sheet_C.cell(row_index, col_index).value 
+         for col_index in xrange(sheet_C.ncols)}
+    if d['Cancer_Risk_Score'] !="":
+    	d.pop("CHEM_E")
+    	d.pop("NonCancer_Risk_Score")
+    	cancer_teps[d['CAS_Number']]=d
+    if e['NonCancer_Risk_Score'] !="":
+    	e.pop("CHEM_E")
+    	e.pop("Cancer_Risk_Score")
+    	noncancer_teps[e['CAS_Number']]=e
+
+f=open("CancerScores", 'wb')
+json.dump(cancer_teps,f)
+g=open("NonCancerScores", 'wb')
+json.dump(noncancer_teps,g)
+
