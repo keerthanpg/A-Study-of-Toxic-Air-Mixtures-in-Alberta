ee3970f40bb047703291ef8405d222614cb11c0f
David <keerthanpg@gmail.com>
2016-07-08 01:35:32 -0600

    Calcnormal corrected

--- FacilityRiskScore.py
+++ FacilityRiskScore.py
@@ -1,5 +1,6 @@
 import json
-
+import os
+os.chdir('C:\Users\David\Desktop\Alberta\A-Study-of-Toxic-Air-Mixtures-in-Alberta')
 f=open('EmissionByFacility.json', 'rb')
 Emissions=json.load(f)
 
@@ -11,20 +12,40 @@ NonCancerScores=json.load(f)
 
 FacilityEmissionScores={}
 for i in xrange(2006,2013):
-    FacilityEmissionScores[str(i)]={}
     for facility in Emissions[str(i)]:
-        FacilityEmissionScores[str(i)][facility]={}
-        FacilityEmissionScores[str(i)][facility]['Cancer_Tonnes']=0.0
-        FacilityEmissionScores[str(i)][facility]['NonCancer_Tonnes']=0.0
-        for discharge in Emissions[str(i)][facility]:
-        	casno=discharge['CAS_Number']
-        	tonnes=discharge['Tonnes_Air']
-        
-        	if casno in CancerScores:
-        		FacilityEmissionScores[str(i)][facility]['Cancer_Tonnes']+=tonnes*CancerScores[casno]
-    		if casno in NonCancerScores:
-    			FacilityEmissionScores[str(i)][facility]['NonCancer_Tonnes']+=tonnes*NonCancerScores[casno]
-    			
+    	for discharge in Emissions[str(i)][facility]:
+            casno=discharge['CAS_Number']
+            tonnes=discharge['Tonnes_Air']
+            if casno in CancerScores:
+                if facility in FacilityEmissionScores:
+                    if str(i) in FacilityEmissionScores[facility]:
+                        if 'Cancer_Tonnes' in  FacilityEmissionScores[facility][str(i)]:
+                            FacilityEmissionScores[facility][str(i)]['Cancer_Tonnes']+=tonnes*CancerScores[casno]
+                        else:
+                            FacilityEmissionScores[facility][str(i)]['Cancer_Tonnes']=tonnes*CancerScores[casno]
+                    else:
+                        FacilityEmissionScores[facility][str(i)]={}
+                        FacilityEmissionScores[facility][str(i)]['Cancer_Tonnes']=tonnes*CancerScores[casno]
+                else:
+                    FacilityEmissionScores[facility]={}
+                    FacilityEmissionScores[facility][str(i)]={}
+                    FacilityEmissionScores[facility][str(i)]['Cancer_Tonnes']=tonnes*CancerScores[casno]
+
+            if casno in NonCancerScores:
+                if facility in FacilityEmissionScores:
+                    if str(i) in FacilityEmissionScores[facility]:
+                        if 'NonCancer_Tonnes' in  FacilityEmissionScores[facility][str(i)]:
+                                FacilityEmissionScores[facility][str(i)]['NonCancer_Tonnes']+=tonnes*NonCancerScores[casno]
+                        else:
+                                FacilityEmissionScores[facility][str(i)]['NonCancer_Tonnes']=tonnes*NonCancerScores[casno]
+                    else:
+                            FacilityEmissionScores[facility][str(i)]={}
+                            FacilityEmissionScores[facility][str(i)]['NonCancer_Tonnes']=tonnes*NonCancerScores[casno]
+                else:
+                        FacilityEmissionScores[facility]={}
+                        FacilityEmissionScores[facility][str(i)]={}
+                        FacilityEmissionScores[facility][str(i)]['NonCancer_Tonnes']=tonnes*NonCancerScores[casno]
+   			
 
 f=open('FacilityRiskScore', 'wb')
 json.dump(FacilityEmissionScores,f)
--- calcnormal.py
+++ calcnormal.py
@@ -6,7 +6,6 @@ from geopy import distance
 from geopy import Point
 import math
 import datetime
-print datetime.datetime.now()
 import matplotlib.pyplot as plt
 
 pi=math.pi
@@ -16,7 +15,7 @@ e=math.e
 mesh=np.load('Points.npy')
 
 f=open('FacilityRiskScore', 'rb')
-Emissions=json.load(f)
+FacilityRiskScores=json.load(f)
 
 f=open('DistancesDic', 'rb')
 FacilityCoordinates=json.load(f)
@@ -25,73 +24,50 @@ FacilityCoordinates=json.load(f)
 var = multivariate_normal(mean=[0,0], cov=[[1,0],[0,1]])
 mesh_years=[mesh, mesh, mesh, mesh, mesh, mesh, mesh, mesh]
 
-nlat=1223
-nlon=660 #no of divisions in longitude
+X=nlat=1223
+Y=nlon=660 #no of divisions in longitude
 
-dellat=(60-49)/nlat
-dellon=(109-120)/nlon
-
-def spiral():
-    X=1223
-    Y=660    
-    x = y = 0
-    dx = 0
-    dy = -1
-    
-    for i in range(10000):
-        if (-X/2 < x <= X/2) and (-Y/2 < y <= Y/2):
-            plt.scatter(x,y)
-            
-
-            # DO STUFF...
-        if x == y or (x < 0 and x == -y) or (x > 0 and x == 1-y):
-            dx, dy = -dy, dx
-        x, y = x+dx, y+dy
-
+dellat=(60.0-49.0)/nlat
+dellon=(109.0-120.0)/nlon
 
 
 print datetime.datetime.now()
-for i in xrange(7):
-	for facility in Emissions[str(i)]:
-		try:
-			source=Point('%s %s' %(FacilityCoordinates[facilityID]['lat'], FacilityCoordinates[facilityID]['lon']))
-			j,k=int ((FacilityCoordinates[facilityID]['lat']-49)/dellat),int((FacilityCoordinates[facilityID]['lon']+109)/dellon) #j and k are lower right nodes\
 
-		except:
-			continue
-	for j in xrange(100):
-		for k in xrange(50):
-			
-			mesh_years[i][j][k][0]=0.0
-			mesh_years[i][j][k][1]=0.0
-			for chemical in Emissions[str(i+2006)]:	
-				try:
-					Cancer_risk=CancerScores[chemical]		
-				except:
-					Cancer_risk=0	
-				try:
-					NonCancer_risk=NonCancerScores[chemical]		
-				except:
-					NonCancer_risk=0
-				l=0
-				for k,v in Emissions[str(i+2006)][chemical].iteritems():
-					facilityID=v['NPRI_ID']	
-
-					print l
-					l=l+1				
-					
+for facilityID in FacilityRiskScores:
+	print facilityID
+	try:
+		source=Point('%s %s' %(FacilityCoordinates[facilityID]['lat'], FacilityCoordinates[facilityID]['lon']))
+		x0,y0=int ((FacilityCoordinates[facilityID]['lat']-49)/dellat),int((FacilityCoordinates[facilityID]['lon']+109)/dellon)#j and k are lower right nodes
+		x = y = 0
+		dx = 0
+		dy = -1
+		for i in range(10000):
+			if (-X/2 < x <= X/2) and (-Y/2 < y <= Y/2):
+				destlat=x+x0
+				destlon=y+y0
+				for year in FacilityRiskScores[facilityID]:
 					try:
-						source=Point('%s %s' %(FacilityCoordinates[facilityID]['lat'], FacilityCoordinates[facilityID]['lon']))
-						dest=Point('%s %s'%(mesh_years[i][j][k][0], mesh_years[i][j][k][1]))
+						dest=Point('%s %s'%(mesh_years[int(year)-2006][destlat][destlon][0], mesh_years[int(year)-2006][destlat][destlon][1]))
 						dist=distance.distance(source,dest).kilometers
-						P=1/(2*pi)*(e**(-dist*dist/2))						
-						mesh_years[i][j][k][0]+=P*v['Tonnes_Air']*1000*Cancer_risk
-						mesh_years[i][j][k][1]+=P*v['Tonnes_Air']*1000*NonCancer_risk
-					except:
+						P=1/(2*pi)*(e**(-dist*dist/2))
+						try:
+							mesh_years[int(year)-2006][destlat][destlon][2]+=P*1000*FacilityRiskScores[facilityID][year]['Cancer_Tonnes']
+						except KeyError:
+							continue
+
+						try:
+							mesh_years[int(year)-2006][destlat][destlon][3]+=P*1000*FacilityRiskScores[facilityID][year]['NonCancer_Tonnes']
+						except KeyError:
+							continue
+					except IndexError:
 						continue
-			print k						
-		print j
-	print i
-					
+					except Exception as e: 
+						print(e)
+			if x == y or (x < 0 and x == -y) or (x > 0 and x == 1-y):
+					dx, dy = -dy, dx
+			x, y = x+dx, y+dy
+	except KeyError:
+		continue
+	
 print datetime.datetime.now()
 np.save("Mesh", mesh_years, allow_pickle=True, fix_imports=True)
